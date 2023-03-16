c1221da25b62faefa7a08bc6be2c591b2145a1ae
nicolasagosti <agostinicolas6557@gmail.com>
2023-03-16 15:58:22 -0300

    mi primer comit

---
+++ app.js
@@ -0,0 +1,10 @@
+const a1 = document.getElementById("a1");
+a1.addEventListener("click", moverA);
+
+const ima1 = document.getElementById("a1");
+const ima3 = document.getElementById("a3");
+const C3 = document.getElementById("3c");
+function moverA(){
+  C3.textContent = "deuna";
+}
+
